e4e802df7418e3bcb42c04452595d134a083243c
dudushy <eduardotalarico1337@gmail.com>
2022-01-05 00:25:26 -0300

    day 2 done

--- day2.py
+++ day2.py
@@ -1,20 +1,52 @@
 import os.path as Path
 import httpx
 from env import sessionCookie
+inputFilePath = None
 
 def part1():
-    pass
+    global inputFilePath
+    with open(inputFilePath, 'r') as file:
+        horizontalPos = 0
+        depth = 0
 
+        for item in file:
+            command, value = item.split()
+            if command == "forward":
+                horizontalPos += int(value)
+            elif command == "down":
+                depth += int(value)
+            else:
+                depth -= int(value)
+
+        print(f"\nWhat do you get if you multiply your final horizontal position by your final depth?\n{horizontalPos * depth}")
 
 def part2():
-    pass
+    global inputFilePath
+    with open(inputFilePath, 'r') as file:
+        horizontalPos = 0
+        depth = 0
+        aim = 0
+
+        for item in file:
+            command, value = item.split()
+            if command == "forward":
+                horizontalPos += int(value)
+                depth += (aim * int(value))
+            elif command == "down":
+                aim += int(value)
+            else:
+                aim -= int(value)
+
+        print(f"\nWhat do you get if you multiply your final horizontal position by your final depth?\n{horizontalPos * depth}")
+
 
 
 def main():
-    pass
+    part1()
+    part2()
 
 if __name__ == "__main__":
-    day : int = 1
+    day : int = 2
     path : str = Path.dirname(__file__)
     inputFilePath : str = Path.join(path, f"day{day}.txt")
 
