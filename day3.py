1f6b7ce32013e33935046ec0a1facdc5b1b32688
dudushy <eduardotalarico1337@gmail.com>
2022-01-05 01:14:47 -0300

    not finished day3

---
+++ day3.py
@@ -0,0 +1,29 @@
+#imports
+import os.path as Path
+
+# set global path
+path = Path.join(Path.dirname(__file__), 'day3.txt')
+
+#main
+def part1():
+    global path
+    with open(path, 'r') as file:
+        gammaRate = 0
+        epsilonRate = 0
+        count1 = 0
+        count0 = 0
+        for line in file:
+            for char in line:
+                pass #enumerate
+
+def part2():
+    global path
+    with open(path, 'r') as file:
+        pass
+
+def main():
+    part1()
+    part2()
+
+if __name__ == "__main__":
+    main()
