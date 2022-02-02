dc5296351fad4feedd850cb52df00e82908e9a3f
dudushy <eduardotalarico1337@gmail.com>
2022-02-02 16:29:18 -0300

    adding main class

---
+++ CSharp/DaySelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    static internal class DaySelector
+    {
+        static void Main(string[] args)
+        {
+            //Day1.part1();
+            //Day1.part2();
+            Day2.part1();
+        }
+    }
+}
